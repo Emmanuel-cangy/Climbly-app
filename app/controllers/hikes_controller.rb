1efd73667dbacad08a302cd9dca5c8a473461902
Emmanuel Cangy <ecangy0102@gmail.com>
2022-06-06 16:08:52 +0400

    added controller for scores and completed controller for hikes

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,2 +1,41 @@
 class HikesController < ApplicationController
+  before_action :set_score, only: %i[show edit update create]
+
+  def index
+    @hikes = Hike.all
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+    @hike = Hike.new
+  end
+
+  def create
+    @hike = hike.new(params_hike)
+    @hike.user = current_user
+    if @hike.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_hikes
+    @score = Score.find(params[:id])
+  end
+
+  def params_hike
+    params.require(:hike).permit(:name)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_06_104740) do
+ActiveRecord::Schema.define(version: 2022_06_06_113953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 2022_06_06_104740) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.datetime "startDay"
+    t.datetime "endDay"
+    t.integer "duration"
+    t.bigint "hike_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hike_id"], name: "index_scores_on_hike_id"
+    t.index ["user_id"], name: "index_scores_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +45,6 @@ ActiveRecord::Schema.define(version: 2022_06_06_104740) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "scores", "hikes"
+  add_foreign_key "scores", "users"
 end
