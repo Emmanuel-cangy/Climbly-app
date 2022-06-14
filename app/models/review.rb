009b5181819cb1c20cafb425426586da91a0f3bc
Emmanuel Cangy <101330128+Emmanuel-cangy@users.noreply.github.com>
2022-06-14 09:23:17 +0400

    Revert "show of reviews"

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :hike
   belongs_to :user
-  has_many_attached :photos
 
   validates :content, :rating, :difficulty, presence: true
   validates :content, length: { minimum: 5 }
